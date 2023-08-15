4f587fd4a941a023701b411fc24a243851fe4d75
De Jong Yeong <dejong.re2@gmail.com>
2023-08-15 13:40:25 +0100

    added logic to extract coupon value

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import json
 from os.path import exists
+from urllib.parse import urlparse, parse_qs
 
 
 def read_qr_code(filename):
@@ -18,34 +19,42 @@ def read_qr_code(filename):
         decode, bbox, _ = detector.detectAndDecode(frame)
 
         if decode:
+            params = parse_qs(urlparse(decode).query)
+            coupon = params.get('coupon', [None])[0]
 
-            # check if code is present in the dictionary
-            if decode in qr_codes:
-                if decode not in scanned_codes:
-                    if qr_codes[decode]:
+            print(coupon)
+
+            # check if coupon is not empty
+            if coupon is not None and coupon != '':
+                # check if code is present in the dictionary
+                if coupon in qr_codes:
+                    if coupon not in scanned_codes:
+                        if qr_codes[coupon]:
+                            # not giving ice cream (show message on screen)
+                            send_to_robot(False)
+                        else:
+                            # qr code is stored bot not used, update dict
+                            qr_codes[coupon] = True
+                            scanned_codes.add(coupon)
+                            with open(filename, "w") as file:
+                                json.dump(qr_codes, file)
+
+                            # send signal to robot and give ice cream
+                            send_to_robot(True)
+                    else:
                         # not giving ice cream (show message on screen)
                         send_to_robot(False)
-                    else:
-                        # qr code is stored bot not used, update dict
-                        qr_codes[decode] = True
-                        scanned_codes.add(decode)
-                        with open(filename, "w") as file:
-                            json.dump(qr_codes, file)
-
-                        # send signal to robot and give ice cream
-                        send_to_robot(True)
+                        print('QR code has already been processed')
                 else:
-                    # not giving ice cream (show message on screen)
-                    send_to_robot(False)
-                    print('QR code has already been processed')
-            else:
-                # qr code is new, not stored and send ice cream
-                qr_codes[decode] = True
-                scanned_codes.add(decode)
-                with open(filename, "w") as file:
-                    json.dump(qr_codes, file)
+                    # qr code is new, not stored and send ice cream
+                    qr_codes[coupon] = True
+                    scanned_codes.add(coupon)
+                    with open(filename, "w") as file:
+                        json.dump(qr_codes, file)
 
-                send_to_robot(True)  # send signal to robot
+                    send_to_robot(True)  # send signal to robot
+            else:
+                print('Unable to extract coupon code')
 
         if bbox is not None and len(bbox) > 0:
             bbox = bbox[0].astype(int)
